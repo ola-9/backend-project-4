6fcb141be832388a3d3094a5548fef83fd507810
ola-9 <nepogodda@gmail.com>
2023-01-14 01:40:56 +0300

    fix linter errors

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -73,10 +73,8 @@ test('errors', async () => {
     .get('/webinars')
     .reply(400);
 
-  expect(async () => {
-    await pageLoader('https://ru.hexlet.io/webinars', tempDir);
-  }).rejects.toThrow();
-  expect(async () => {
-    await pageLoader('https://ru.hexlet.io/webinars', '/none-exist');
-  }).rejects.toThrow();
+  await expect(() => pageLoader('https://ru.hexlet.io/webinars', tempDir))
+    .rejects.toThrow();
+  await expect(() => pageLoader('https://ru.hexlet.io/webinars', '/none-exist'))
+    .rejects.toThrow();
 });
