1afbbb764d6577a1a55690d47e0c478ca7f9089b
ola-9 <nepogodda@gmail.com>
2023-01-17 23:19:35 +0300

    fix tests: add persist-false

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -39,6 +39,7 @@ beforeEach(async () => {
 
 test('page and resources are downloaded', async () => {
   nock('https://ru.hexlet.io')
+    .persist()
     .get('/courses')
     .reply(200, initial)
     .get('/assets/professions/nodejs.png')
@@ -46,7 +47,8 @@ test('page and resources are downloaded', async () => {
     .get('/assets/application.css')
     .reply(200, expectedImg)
     .get('/packs/js/runtime.js')
-    .reply(200, expectedImg);
+    .reply(200, expectedImg)
+    .persist(false);
 
   // https://stackoverflow.com/questions/48674675/error-nock-no-match-for-request
   nock('https://ru.hexlet.io')
